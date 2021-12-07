9cad17246715a568ac56d350e528033209bc8dd4
Sharkkii <jaws.sharkkii@gmail.com>
2021-12-07 15:17:17 +0900

    add test script
    - add `test_add_helper`
    check `RecordHelper` and `VisualizationHelper`

--- test/test_add_helper.py
+++ test/test_add_helper.py
@@ -1,38 +1,55 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
 
-from src.helper import Helper, VisualizationHelper
+from src.helper import RecordHelper
+from src.helper import VisualizationHelper
 
-def test_add_helper():
+def test_add_record_helper():
 
-    root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    helper = Helper(
+    root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../dest'))
+    helper = RecordHelper(
         root = root
     )
-    dest = helper.dest
-    project_root = os.path.join(dest, "project")
-    print(project_root)
+    helper.setup()
+
+    for _ in range(10):
+        r = np.random.randn()
+        helper.record("random variable A", r)
+        helper.record("random variable B", r)
+    helper.save_to_csv("result")
+    helper.save_to_json("result")
+    data = helper.load_from_csv("result")
+    print(data)
+    data = helper.load_from_json("result")
+    print(data)
+
+    print("OK: test_add_record_helper")
+
+def test_add_visualization_helper():
+
+    root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../dest"))
     vhelper = VisualizationHelper(
-        root = project_root
+        root = root
     )
     vhelper.setup()
 
     data = {"data": [3, 1, 4, 1, 5]}
     print(data)
     graph = vhelper.line(data)
-    vhelper.save_csv_data(data)
-    vhelper.save_json_data(data)
-    vhelper.save_graph(graph)
+    vhelper.save_graph(
+        graph,
+        path = "result"
+    )
+    image = vhelper.load_graph("result")
+    vhelper.show_graph("result")
 
-    data = vhelper.load_csv_data()
-    print(data)
-    data = vhelper.load_json_data()
-    print(data)
-    image = vhelper.load_graph()
-    vhelper.show_graph(image)
+    print("OK: test_add_visualization_helper")
 
-    print("OK: test_add_helper")
+def test_add_helper():
+    test_add_record_helper()
+    test_add_visualization_helper()
 
 def main():
     test_add_helper()
