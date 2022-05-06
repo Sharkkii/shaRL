f7bfa2c1e6f40c4a738c4daaab36fb41ca6a3abf
Toshiki Kodera <jaws.sharkkii@gmail.com>
2022-05-06 15:08:33 +0900

    add test cases for Environment & GymEnvironment

---
+++ test/main/test_environment.py
@@ -0,0 +1,96 @@
+import pytest
+import numpy as np
+import gym
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
+
+from src.environment import Environment
+from src.environment import GymEnvironment
+
+@pytest.mark.L2
+class TestEnvironment():
+
+    @pytest.mark.unit
+    def test_should_be_unavailable_on_initialization(self):
+        env = Environment()
+        assert env.is_available == False
+
+    @pytest.mark.unit
+    def test_should_be_available_after_setup(self):
+        env = Environment()
+        env.setup()
+        assert env.is_available == True
+
+    @pytest.mark.unit
+    def test_should_have_observation_space(self):
+        env = Environment()
+        env.setup()
+        assert isinstance(env.observation_space, gym.Space)
+
+    @pytest.mark.unit
+    def test_should_have_action_space(self):
+        env = Environment()
+        env.setup()
+        assert isinstance(env.action_space, gym.Space)
+
+    @pytest.mark.unit
+    def test_reset_method_should_return_single_observation(self):
+        env = Environment()
+        env.setup()
+        observation = env.reset()
+        assert type(observation) is np.ndarray
+
+    @pytest.mark.unit
+    def test_sample_method_should_return_single_action(self):
+        env = Environment()
+        env.setup()
+        _ = env.reset()
+        action = env.sample()
+        assert action is not None
+    
+    @pytest.mark.unit
+    def test_step_method_should_return_tuple_of_obs_r_done_info(self):
+        env = Environment()
+        env.setup()
+        action = env.sample()
+        observation, reward, done, info = env.step(action)
+        assert type(observation) is np.ndarray
+        assert type(reward) in (int, float)
+        assert type(done) is bool
+
+@pytest.mark.L2
+class TestGymEnvironment():
+
+    @pytest.mark.unit
+    def test_should_be_available_on_empty_initialization(self):
+        env = GymEnvironment()
+        assert env.is_available == False
+
+    @pytest.mark.unit
+    def test_should_be_available_on_valid_initialization(self):
+        env = GymEnvironment(
+            name = "CartPole-v1"
+        )
+        assert env.is_available == True
+
+    @pytest.mark.unit
+    def test_should_raise_value_error_on_invalid_intialization(self):
+        with pytest.raises(ValueError) as message:
+            env = GymEnvironment(
+                name = "INVALID_NAME"
+            )
+
+    @pytest.mark.unit
+    def test_should_be_available_after_valid_setup(self):
+        env = GymEnvironment()
+        env.setup(
+            name = "CartPole-v1"
+        )
+        assert env.is_available == True
+
+    def test_should_raise_value_error_after_invalid_setup(self):
+        env = GymEnvironment()
+        with pytest.raises(ValueError) as message:
+            env = GymEnvironment(
+                name = "INVALID_NAME"
+            )
