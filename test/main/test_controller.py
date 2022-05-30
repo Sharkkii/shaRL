0da0c8d5b570816dc4cad04937b72e5c90f3d905
Toshiki Kodera <jaws.sharkkii@gmail.com>
2022-05-30 16:29:29 +0900

    add tests for `GoalConditionedRLController`

--- test/main/test_controller.py
+++ test/main/test_controller.py
@@ -5,8 +5,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from src.const import SpaceType
 from src.common import AgentInterface
 from src.environment import Environment
+from src.environment import GoalReachingTaskEnvironment
 from src.agent import Agent
-from src.controller import Controller
+from src.agent import GoalConditionedAgent
+from src.controller import RLController
+from src.controller import GoalConditionedRLController
 
 
 default_agent_interface = AgentInterface(
@@ -21,14 +24,14 @@ class TestController():
 
     @pytest.mark.unit
     def test_should_be_unavailable_on_empty_initialization(self):
-        controller = Controller()
+        controller = RLController()
         assert controller.is_available == False
 
     @pytest.mark.unit
     def test_should_be_available_on_nonempty_initialization(self):
         env = Environment()
         agent = Agent()
-        controller = Controller(
+        controller = RLController(
             environment = env,
             agent = agent
         )
@@ -38,7 +41,7 @@ class TestController():
     def test_should_be_available_after_setup(self):
         env = Environment()
         agent = Agent()
-        controller = Controller()
+        controller = RLController()
         controller.setup(
             environment = env,
             agent = agent
@@ -53,7 +56,59 @@ class TestController():
             interface = default_agent_interface,
             use_default = True
         )
-        controller = Controller()
+        controller = RLController()
+        controller.setup(
+            environment = env,
+            agent = agent
+        )
+        controller.train(
+            n_epoch = 1,
+            n_env_step = 1,
+            n_gradient_step = 1,
+            max_dataset_size = 1000,
+            batch_size = 10,
+            shuffle = False
+        )
+
+
+@pytest.mark.L1
+class TestGoalConditionedRLController():
+
+    @pytest.mark.unit
+    def test_should_be_unavailable_on_empty_initialization(self):
+        controller = GoalConditionedRLController()
+        assert controller.is_available == False
+
+    @pytest.mark.unit
+    def test_should_be_available_on_nonempty_initialization(self):
+        env = GoalReachingTaskEnvironment()
+        agent = GoalConditionedAgent()
+        controller = GoalConditionedRLController(
+            environment = env,
+            agent = agent
+        )
+        assert controller.is_available == True
+
+    @pytest.mark.unit
+    def test_should_be_available_after_setup(self):
+        env = GoalReachingTaskEnvironment()
+        agent = GoalConditionedAgent()
+        controller = GoalConditionedRLController()
+        controller.setup(
+            environment = env,
+            agent = agent
+        )
+        assert controller.is_available == True
+
+    @pytest.mark.unit
+    @pytest.mark.integration
+    def test_train_method_should_work(self):
+        env = GoalReachingTaskEnvironment()
+        agent = GoalConditionedAgent(
+            interface = default_agent_interface,
+            use_default = True
+        )
+        controller = GoalConditionedRLController()
         controller.setup(
             environment = env,
             agent = agent
