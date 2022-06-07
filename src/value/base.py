11ffafa26342ddded0d3f64b9c3cb4bafcf0b090
Toshiki Kodera <jaws.sharkkii@gmail.com>
2022-06-07 12:10:06 +0900

    split `value.py` into 3 files

---
+++ src/value/base.py
@@ -0,0 +1,77 @@
+#### Value (Base Class) ####
+
+from abc import ABC, abstractmethod
+
+
+class ValueBase(ABC):
+
+    @abstractmethod
+    def __init__(self): raise NotImplementedError
+    @abstractmethod
+    def setup(self): raise NotImplementedError
+    # @abstractmethod
+    # def setup_with_policy(self): raise NotImplementedError
+    # @abstractmethod
+    # def reset(self): raise NotImplementedError
+    @abstractmethod
+    def __call__(self): raise NotImplementedError
+    # @abstractmethod
+    # def update(self): raise NotImplementedError
+    @abstractmethod
+    def train(self): raise NotImplementedError
+    @abstractmethod
+    def eval(self): raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def interface(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def configuration(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def value_network(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def value_optimizer(self): raise NotImplementedError
+
+
+class QValueBase(ABC):
+
+    @abstractmethod
+    def __init__(self): raise NotImplementedError
+    @abstractmethod
+    def setup(self): raise NotImplementedError
+    # @abstractmethod
+    # def setup_with_policy(self): raise NotImplementedError
+    # @abstractmethod
+    # def reset(self): raise NotImplementedError
+    @abstractmethod
+    def __call__(self): raise NotImplementedError
+    # @abstractmethod
+    # def update(self): raise NotImplementedError
+    @abstractmethod
+    def train(self): raise NotImplementedError
+    @abstractmethod
+    def eval(self): raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def interface(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def configuration(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def qvalue_network(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def qvalue_optimizer(self): raise NotImplementedError
+
+
+class DiscreteQValueBase(QValueBase):
+    pass
+
+
+class ContinuousQValueBase(QValueBase):
+    pass
