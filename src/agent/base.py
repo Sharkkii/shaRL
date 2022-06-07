0aa53345956847f59403894b7a931a6eb3b985f6
Toshiki Kodera <jaws.sharkkii@gmail.com>
2022-06-07 11:49:54 +0900

    split `agent.py` into 3 files
    - base class `base.py`
    - mixin class `mixin.py`
    - entity `agent.py`

---
+++ src/agent/base.py
@@ -0,0 +1,62 @@
+#### Agent (Base Class) ####
+
+from abc import ABC, abstractmethod
+
+
+class AgentBase(ABC):
+
+    @abstractmethod
+    def __init__(self): raise NotImplementedError
+    @abstractmethod
+    def setup(self): raise NotImplementedError
+    # TODO) TO BE RENAMED: setup_on_every_epoch #
+    @abstractmethod
+    def setup_on_every_epoch(self): raise NotImplementedError
+    # TODO) TO BE RENAMED: setup_on_every_step #
+    @abstractmethod
+    def setup_on_every_step(self): raise NotImplementedError
+    # @abstractmethod
+    # def reset(self): raise NotImplementedError
+    @abstractmethod
+    def choose_action(self): raise NotImplementedError
+    @abstractmethod
+    def interact_with_env(self): raise NotImplementedError
+    @abstractmethod
+    def update_actor(self): raise NotImplementedError
+    @abstractmethod
+    def update_critic(self): raise NotImplementedError
+    @abstractmethod
+    def train(self): raise NotImplementedError
+    @abstractmethod
+    def eval(self): raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def interface(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def configuration(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def actor(self): raise NotImplementedError
+    @property
+    @abstractmethod
+    def critic(self): raise NotImplementedError
+    # @property
+    # @abstractmethod
+    # def env(self): raise NotImplementedError
+    # @property
+    # @abstractmethod
+    # def memory(self): raise NotImplementedError
+
+
+class DiscreteControlAgentBase(AgentBase):
+    pass
+
+
+class ContinuousControlAgentBase(AgentBase):
+    pass
+
+
+class GoalConditionedAgentBase(AgentBase):
+    pass
